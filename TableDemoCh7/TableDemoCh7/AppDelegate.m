1ed538117ae36e5d6a86a4dfe00b2341c5e785d5
Kate Sohng <kheeya7@hotmail.com>
2018-02-27 20:24:37 -0800

    Making conflict -2

--- TableDemoCh7/TableDemoCh7/AppDelegate.m
+++ TableDemoCh7/TableDemoCh7/AppDelegate.m
@@ -16,7 +16,7 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    NSLog(@"String for the test");
     return YES;
 }
 
